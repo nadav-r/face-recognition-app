167a71006f6b4a39e98270c59709e33804cb8f61
nadav-r <36492247+nadav-r@users.noreply.github.com>
2019-04-21 16:02:55 +0300

    changed port

--- facerecognitionbrain-api/server.js
+++ facerecognitionbrain-api/server.js
@@ -21,7 +21,7 @@ const db=knex({
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req,res)=> {res.send(database.users);})
+app.get('/',(req,res)=> {res.send("it is working");})
 
 app.post('/signin',(req,res)=>{signin.handleSignin(req,res,db,bcrypt)});
 
@@ -34,7 +34,7 @@ app.put('/image', (req,res) =>{image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)});
 
 app.listen(process.env.PORT || 3000,()=>{
-	console.log('app is running on port 3000');
+	console.log(`app is running on port ${process.env.port}`);
 });
 
 
